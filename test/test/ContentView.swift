285296fbdc5dc6565f1313a106ce313fe51a5e22
Abdulwahab Alqallaf <nnoz0c@gmail.com>
2022-03-03 14:54:29 +0300

    done cw2

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Abdulwahab Alqallaf on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, wahab!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
